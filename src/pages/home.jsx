d880f22343db86152339c9fda9bcfd85d08bc1d5
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2023-09-30 22:18:10 +0500

    added autocomlete

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,9 @@
+import { TestCombo } from "../components/autocomplete"
 export default function Home(){
     return(
         <div>
             I am Home
+            <TestCombo/>
         </div>
     )
 }
