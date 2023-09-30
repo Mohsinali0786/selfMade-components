615cc69d10f9ed13880452516abca80995973664
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2023-09-30 18:17:50 +0500

    correction

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,7 @@
-// import { Calender } from "../components/calender/calender";
-import CustomCheckbox from "../components/checkbox";
-// import DataGridDemo from "../components/grid/grid";
 export default function Home(){
     return(
         <div>
-            {/* <Calender maxwidth="maxx-width"  dateCount={10}/> */}
-            {/* <DataGridDemo/> */}
-            <CustomCheckbox 
-                defaultChecked={true}
-                disabled={false}
-                lable="Test"
-                required={true}
-                color="success"
-                colorbeforeChecked="red"
-                colorafterChecked="yellow"
-                indeterminate={true}
-                method={()=>console.log('Clicked')}
-                // icon={<BookmarkBorderIcon />}
-                // checkedIcon={<BookmarkIcon />}
-            />
+            I am Home
         </div>
     )
 }
