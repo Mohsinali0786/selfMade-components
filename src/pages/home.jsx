9afb543e263fb37138acf750f3bff1e4e1dc36c9
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2023-09-30 19:45:05 +0500

    added input

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,11 @@
+import { TestInput } from "../component/input"
+import { PasswordIcon } from "../component/passwordIcon"
 export default function Home(){
     return(
         <div>
             I am Home
+            <TestInput/>
+            <PasswordIcon/>
         </div>
     )
 }
