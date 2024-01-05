e97312f5f92a90b8f65d0d93c941e325d68e9184
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-01-05 09:25:22 +0500

    pending code

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,10 @@
+import { Header, TestHeader } from "../components/Header/header";
+
 export default function Home(){
     return(
         <div>
             I am Home
+            <TestHeader/>
         </div>
     )
 }
